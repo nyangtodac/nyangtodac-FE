fbd3e46bc06cf056231540dfd7cb4ac8b410795f
taewoongheo <chamjoeun0111@gmail.com>
2025-12-05 18:43:11 +0900

    chore(expo): eslint 및 prettier 설정

--- apps/expo/eslint.config.js
+++ apps/expo/eslint.config.js
@@ -1,10 +1,17 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
+const { defineConfig, globalIgnores } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const globals = require('globals');
 
 module.exports = defineConfig([
+  globalIgnores(['dist/*', '/.expo', 'node_modules']),
   expoConfig,
+  eslintPluginPrettierRecommended,
   {
-    ignores: ['dist/*'],
+    files: ['babel.config.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
   },
 ]);
